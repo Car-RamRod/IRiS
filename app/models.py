d70af631d6b102218924ffcb9e0b7634b65a6607
Jan Masztal <janmasztal@mail.adelphi.edu>
2015-05-10 16:57:48 -0400

    resource classes

--- app/models.py
+++ app/models.py
@@ -1,47 +1,66 @@
 from mongoalchemy.session import Session
 from mongoalchemy.document import Document, Index
 from mongoalchemy.fields import *
-
+from datetime import  datetime
 
 class Alert(Document):
 	config_collection_name = 'alert'
 
-	title = StringField()
+	resource_type = StringField()
+	source = StringField()
 	status = StringField()
-	atype = ListField(AnythingField())
-#	alert_generated = DateTimeField()
-	entered = DateTimeField()
-	ip = ListField(AnythingField())
-        mac = ListField(AnythingField())
-	comments = ListField(AnythingField())
+	timestamp = DateTimeField()
+	comments = ListField(AnythingField())	
 
-	def __str__(self):
-		return '%s %s %s' % (self.title, self.status,self.ip)
+	def promote(self):
+		source = Source(self, self.source)
+		return source
+
+class IPSAlert(Alert):
+	ip_src = StringField()
+	ip_dst = StringField()
+	port_src = StringField()
+	port_dst = StringField()
+	
+	def promote(self):	
+		socket = Socket(self, self.ip_src, self.port_src)
+		return socket
 
 
+class Resource(Field):
+        creation_datetime = None
+        creator = None
+
+
+
+
+class Source(Resource):
+        alert_id = None
+        source = None
+
+        def __init__(self,parent,source):
+                self.alert_id = parent.mongo_id
+		self.source = source
+
+class Socket(Resource):
+	alert_id = None
+	ip = None
+	port = None
+
+	def __init__(self, parent,  ip, port):
+		self.alert_id = parent.mongo_id
+		self.ip = ip
+		self.port = port
+	
+	
 class Incident(Document):
 	config_collection_name = 'incident'
-	
-	#incident_title = StringField()
-	title = StringField()
-	status = StringField()
-	##idNum = StringField()
-	itype = StringField()
-	#itype = ListField(AnythingField())
-	#incident_generated = DateTimeField()
-	entered = DateTimeField()
-	#incident_ip = ListField(AnythingField())
-	#incident_mac = ListField(AnythingField())
-	
-	comments = ListField(AnythingField())
-	
-	alerts = ListField(RefField(type=DocumentField(Alert)))
-	
+	resource = ListField(AnythingField())
 
 
 iris_db = Session.connect('iris_db')
 
 #will clear collection when python  run.py
-#iris_db.clear_collection(Alert)
-#iris_db.clear_collection(Incident)
+iris_db.clear_collection(Alert)
+iris_db.clear_collection(Incident)
 
